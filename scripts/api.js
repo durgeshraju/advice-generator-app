89e5683fb74c83f0375de86f84ab4f51ad2f15d1
Durgesh <durgeshnidadavolu@gmail.com>
2026-03-14 17:26:23 +0800

    get api call setup
    - Fetch random advice from Advice Slip API
    - Handle HTTP errors with response.ok check
    - Bypass browser cache with cache: 'no-cache'
    - Return null on failure for safe error handling in main.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,19 @@
 
 // API Endpoint URL
 const API_URL = "https://api.adviceslip.com/advice";
+
+
+export async function fetchAdvice() {
+    
+    try{
+        const response = await fetch(API_URL, {cache: 'no-cache'});        
+        if(!response.ok){
+            throw new Error('Response error')
+        }        
+        const data = await response.json();
+        return data
+    } catch(error){
+        console.error('Error Message:', error);
+        return null;
+    }
+}
