6a7f391d4b50a4cddc9652d0f4c5601fe298e066
Durgesh <durgeshnidadavolu@gmail.com>
2026-03-14 17:25:31 +0800

    Grab the DOM elements

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 import {fetchAdvice} from './api.js';
+
+// Grab the DOM elements
+
+const diceBtn = document.querySelector('.dice-button');
+const adviceId = document.querySelector('.advice-id');
+const adviceText = document.querySelector('.advice-text');
